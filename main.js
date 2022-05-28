ba8f271bca2c0d527b1734d4bb021b7cb4ae88f2
MKSRM <95507826+MKSRM@users.noreply.github.com>
2022-05-28 13:52:24 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -17,14 +17,14 @@ menu_list_array = ["Veg Margherita Pizza",
 
     function add_item(){
         var htmldata;
-		var imgtags='<img id="im1" src="pizzaImg.png">'
-        var item=document.getElementById("add_item").value;
+		var imgtags='<img id="im1" src="pizzaImg.png">';
         // use the sort function as - menu_list_array.sort();
-        htmldata="menu_list_array.sort()";
+        menu_list_array.sort();
+        htmldata=" ";
         for(var i=0;i<menu_list_array.length;i++){
             htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
         }
-         document.getElementById("display_addedmenu").innerHTML = htmldata;
+        document.getElementById("display_addedmenu").innerHTML = htmldata;
 		
     }
 
